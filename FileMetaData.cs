7f0779b358ceb68321918a29008a5eeae118f57a
alinc <alinc@DESKTOP-G7RINBJ>
2019-09-02 23:04:17 -0500

    Refactor FileMetaData Class

--- FileMetaData.cs
+++ FileMetaData.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +13,52 @@ namespace SignatureAnalysis
         public string FilePath { get; set; }
         public string FileType { get; set; }
         public string MD5Hash { get; set; }
+
+        public FileMetaData(string filePath)
+        {
+            FilePath = filePath;
+            FileType=CalculateFileType(filePath);
+            MD5Hash = ExtractMD5Hash(filePath);
+        }
+        private  string CalculateFileType(string path)
+        {
+            byte[] doc = File.ReadAllBytes(path);
+            if (doc.Length > 2)
+            {
+                byte doc1 = doc[0];
+                byte doc2 = doc[1];
+                byte doc3 = doc[2];
+                byte doc4 = doc[3];
+
+                //JPG file
+                if (Convert.ToByte("FF", 16) == doc1 && Convert.ToByte("D8", 16) == doc2)
+                {
+                    return "JPG";
+                }
+                //PDF file
+                else if (Convert.ToByte("25", 16) == doc1 && Convert.ToByte("50", 16) == doc2 &&
+                         Convert.ToByte("44", 16) == doc3 && Convert.ToByte("46", 16) == doc4)
+                {
+                    return "PDF";
+                }
+            }
+
+            return null;
+
+        }
+
+        public static string ExtractMD5Hash(string file)
+        {
+            byte[] doc = File.ReadAllBytes(file);
+            MD5 md5 = MD5.Create();
+            byte[] hash = md5.ComputeHash(doc);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
 
     }
 }
--- Search.cs
+++ Search.cs
@@ -16,10 +16,7 @@ namespace SignatureAnalysis
             string[] allmyFiles = Directory.GetFiles(directory);
             foreach (string aF in allmyFiles)
             {
-                FileMetaData myfile = new FileMetaData();
-                myfile.FilePath = aF;
-                myfile.FileType = CalculateFileType(aF);
-                myfile.MD5Hash = ExtractMD5Hash(aF);
+                FileMetaData myfile = new FileMetaData(aF);
                 if (!string.IsNullOrEmpty(myfile.FileType))
                 {
                     allFiles.Add(myfile);
@@ -41,47 +38,6 @@ namespace SignatureAnalysis
                 }
             }
             return allFiles;
-        }
-
-
-        private static string CalculateFileType(string path)
-        {
-            byte[] doc = File.ReadAllBytes(path);
-            if (doc.Length > 2)
-            {
-                byte doc1 = doc[0];
-                byte doc2 = doc[1];
-                byte doc3 = doc[2];
-                byte doc4 = doc[3];
-
-                //JPG file
-                if (Convert.ToByte("FF", 16) == doc1 && Convert.ToByte("D8", 16) == doc2)
-                {
-                    return "JPG";
-                }
-                //PDF file
-                else if (Convert.ToByte("25", 16) == doc1 && Convert.ToByte("50", 16) == doc2 &&
-                         Convert.ToByte("44", 16) == doc3 && Convert.ToByte("46", 16) == doc4)
-                {
-                    return "PDF";
-                }
-            }
-
-            return null;
-
-        }
-
-        public static string ExtractMD5Hash(string file)
-        {
-            byte[] doc = File.ReadAllBytes(file);
-            MD5 md5 = MD5.Create();
-            byte[] hash = md5.ComputeHash(doc);
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < hash.Length; i++)
-            {
-                sb.Append(hash[i].ToString("X2"));
-            }
-            return sb.ToString();
         }
     }
 }
